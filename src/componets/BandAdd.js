2e5296bdc7c4522b0bda6fbbc07fde68da0bb68e
Gerardo Miranda <gerardodev25@gmail.com>
2022-05-24 17:53:22 -0400

    first commit

---
+++ src/componets/BandAdd.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BandAdd = () => {
+  return <div>BandAdd</div>;
+};
+
+export default BandAdd;
---
+++ src/componets/BandList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BandList = () => {
+  return <div>BandList</div>;
+};
+
+export default BandList;
